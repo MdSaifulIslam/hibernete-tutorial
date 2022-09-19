467f97926599a40fbe3a172a1e02c18c142c38f1
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-09-19 16:07:33 +0600

    Data inserted

---
+++ src/com/springdemo/hibernet/demo/CreateStudentDemo.java
@@ -0,0 +1,33 @@
+package com.springdemo.hibernet.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.springdemo.hibernet.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		try {
+			Session session = factory.getCurrentSession();
+
+			Student tempStudent = new Student("Asif", "Saharior", "asif@gmail.com");
+
+			session.beginTransaction();
+			session.save(tempStudent);
+			session.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
